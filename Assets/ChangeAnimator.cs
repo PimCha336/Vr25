c1c62c6c2cae209bedddab13c7a7cfcf4ec1f693
PimCha336 <chamarat2000@gmail.com>
2022-02-26 04:08:11 +0700

    first commit

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    
+    public void getAnimatorTwerk(){
+       this.GetComponent<Animator>().Play("Dancing Twerk");
+    }
+    public void getAnimatorRumba(){
+       this.GetComponent<Animator>().Play("Rumba Dancing");
+    }
+
+}
